ec232a7551826c5fde2140dcc9f3c9235c2cbb2c
Kaio Vieira <kaiomassevieira@gmail.com>
2024-12-25 02:27:50 -0300

    Uploaded the correct day19/part2

--- day19/part2.cpp
+++ day19/part2.cpp
@@ -28,55 +28,32 @@ string trim(string s) {
 }
 
 int main() {
-  const int N = 71;
-  vector<pair<int, int>> bytes;
-
   string s;
-  while (getline(cin, s)) {
-    int x = stoi(split(s, ',')[0]);
-    int y = stoi(split(s, ',')[1]);
-    bytes.push_back({x, y});
+  getline(cin, s);
+
+  vector<string> patterns = split(s, ',');
+  for (auto &x : patterns) {
+    x = trim(x);
   }
 
-  vector<int> dx = {0, 1, 0, -1};
-  vector<int> dy = {1, 0, -1, 0};
+  ll ans = 0;
+  while (getline(cin, s)) {
+    if (s.empty()) {continue;}
 
-  auto check = [&](int qt) {
-    vector grid(N, vector<int>(N, 0));
-    for (int i = 0; i < qt; i++) {
-      auto [x, y] = bytes[i];
-      grid[x][y] = 1;
-    }
+    int n = s.size();
+    vector dp(n + 1, 0ll);
+    dp[n] = 1;
 
-    vector dist(N, vector<int>(N, N * N));
-    queue<pair<int, int>> q;
-    dist[0][0] = 0;
-    q.push({0, 0});
-    while (!q.empty()) {
-      auto [x, y] = q.front();
-      q.pop();
-      for (int i = 0; i < 4; i++) {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-          continue;
-        if (grid[nx][ny] == 1)
-          continue;
-        if (dist[nx][ny] > dist[x][y] + 1) {
-          dist[nx][ny] = dist[x][y] + 1;
-          q.push({nx, ny});
+    for (int i = n - 1; i >= 0; i--) {
+      for (auto &pattern : patterns) {
+        int m = pattern.size();
+        if (i + m <= n && s.substr(i, m) == pattern) {
+          dp[i] += dp[i + m];
         }
       }
     }
-    return dist[N - 1][N - 1] == N*N;
-  };
-
-  int lo = 1, hi = bytes.size();
-  while (lo < hi) {
-    int mid = (lo + hi) / 2;
-    if (check(mid)) hi = mid;
-    else lo = mid + 1;
+    ans += dp[0];
   }
+  cout << ans << endl;
 
-  cout << bytes[lo - 1].first << " " << bytes[lo - 1].second << endl;
 }
